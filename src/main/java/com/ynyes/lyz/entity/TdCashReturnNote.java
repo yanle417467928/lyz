050f6170a9b94cb85ccba41b410b5e559997ccbb
Mr.D <dx631231@sina.com>
2016-06-16 12:36:49 +0800

    修改bug

---
+++ src/main/java/com/ynyes/lyz/entity/TdCashReturnNote.java
@@ -0,0 +1,35 @@
+package com.ynyes.lyz.entity;
+//package com.ynyes.lyz.interfaces.entity;
+//
+//import java.util.Date;
+//
+//import javax.persistence.Column;
+//import javax.persistence.Entity;
+//import javax.persistence.GeneratedValue;
+//import javax.persistence.GenerationType;
+//import javax.persistence.Id;
+//
+//import org.springframework.format.annotation.DateTimeFormat;
+//
+///**
+// * <p>标题：TdCashReturnNote.java</p>
+// * <p>描述：第三方退款单据，只有在需要退还支付宝、微信、银联金额的时候才能够退还</p>
+// * <p><p>公司：http://www.ynsite.com</p></p>
+// * @author 作者：DengXiao
+// * @version 版本：上午11:52:39
+// */
+//@Entity
+//public class TdCashReturnNote {
+//
+//	//主键
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.AUTO)
+//	private Long id;
+//	
+//	//生成时间
+//	@Column
+//	@DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//	private Date createTime;
+//	
+//	
+//}
